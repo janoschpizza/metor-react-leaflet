3403a7ff80d614692841abe561c5fd4bb4b9483b
Joel <Joel@TheCompany.nu>
2016-09-01 23:08:29 +0800

    initial commit

---
+++ client/main.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+
+class App extends Component {
+
+  render () {
+    const position = [51.505, -0.09]
+    return (
+      <Map center={position} zoom={13}>
+        <TileLayer
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>
+            <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+          </Popup>
+        </Marker>
+      </Map>
+    )
+  }
+}
+
+
+Meteor.startup(() => {
+  render(<App/>, document.getElementById('render-target'));
+})
