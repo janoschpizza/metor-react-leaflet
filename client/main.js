1d0d37d35985982cec0481bfcacd39576cb862b8
Joel <Joel@TheCompany.nu>
2016-09-21 11:55:28 +0800

    Switching to React and ES6

--- client/main.js
+++ client/main.js
@@ -3,25 +3,36 @@ import { render } from 'react-dom'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      lat: 51.505,
+      lng: -0.09,
+      zoom: 13,
+    }
+  }
 
   render () {
     const position = [51.505, -0.09]
     return (
-      <Map center={position} zoom={13}>
-        <TileLayer
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker position={position}>
-          <Popup>
-            <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
-          </Popup>
-        </Marker>
-      </Map>
+      <div id="app-container">
+        <Map center={position} zoom={13}>
+          <TileLayer
+            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          {
+            // <Marker position={position}>
+            //   <Popup>
+            //     <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+            //   </Popup>
+            // </Marker>
+          }
+        </Map>
+      </div>
     )
   }
 }
-
 
 Meteor.startup(() => {
   render(<App/>, document.getElementById('render-target'));
---
+++ client/main.test.js
@@ -0,0 +1,16 @@
+import { assert } from 'chai'
+
+describe('Test #1', function () {
+  it('is true', function () {
+    assert.isTrue(true)
+  })
+  it('is false', function () {
+    assert.isFalse(false)
+  })
+})
+
+// describe('<App/>', function () {
+//   it('is instance of Component', function () {
+//     assert.instanceOf(App, Component)
+//   })
+// })
